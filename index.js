cec7bfe2145ca2d21075bcbc52f3f16fb82cbd05
Levi Frank <LeviOrionFrank@gmail.com>
2022-05-30 09:15:53 -0400

    It plays sounds now!

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("ssssuuuuupppp  :D");
+window.addEventListener('keydown', function(e) { 
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.play();
+});
