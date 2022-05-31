83b23aed856f16822ac96894573770fea43a0fcd
Levi Frank <LeviOrionFrank@gmail.com>
2022-05-30 21:07:42 -0400

    now done!

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 window.addEventListener('keydown', function(e) { 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio) return;
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;  //if no audio, stop the function from happening any more
+    audio.currentTime = 0;  //rewind to the start
     audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    console.log(e.propertyName);
+    this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
